1452c8d08111d742b46228ad81385943b1108176
minko <minko@MINKO-DEV-PC>
2023-09-05 15:03:28 +0300

    Sales BR added

--- Validation/Program.cs
+++ Validation/Program.cs
@@ -29,7 +29,7 @@ namespace Validation
             string strWorkPath = System.IO.Path.GetDirectoryName(strExeFilePath);
 
             //Switch Off FA Domain debug
-            bool FADomainDebug = true;
+            bool FADomainDebug = false;
             List<string> SysList = new List<string>();
             if (FADomainDebug)
             {
@@ -320,11 +320,7 @@ namespace Validation
                 #endregion
 
                 //TODO: zarejdaneto na pravilata da stava pri startirane na api i da se dyrvat kato struktura w pametta
-
-                
-
-               
-
+                  
                 foreach (var rule in FaBrDef)
                 {
                     switch (rule.Code)
@@ -12184,7 +12180,139 @@ namespace Validation
             }
 
 
+            bool SalesDomainDebug = true;
+            if (SalesDomainDebug)
+            {
+                #region Sales Report
+                string filePathSalesReport = strWorkPath + @"\FluxReports\Samples_Sales_SN_Report_Message_Normal.xml";
+                XmlDocument xmlDoc = new XmlDocument();
 
+                if (File.Exists(filePathSalesReport))
+                {
+                    xmlDoc.Load(filePathSalesReport);
+                }
+
+                XDocument xdoc = XDocument.Load(filePathSalesReport);
+                string jsonText = JsonConvert.SerializeXNode(xdoc);
+                //var sdas  = xdoc.ToString();
+                var stringReader = new System.IO.StringReader(xdoc.ToString());
+                var serializer = new XmlSerializer(typeof(FLUXSalesReportMessageType));
+                FLUXSalesReportMessageType SalesReport = serializer.Deserialize(stringReader) as FLUXSalesReportMessageType;
+
+                #region read csv Sales BRules
+                string fPathSalesBRules = strWorkPath + @"\FluxReports\SALESBRDEF.csv";
+                List<Sales_MDR_Sales_BR_Def> SalesBrDef = new List<Sales_MDR_Sales_BR_Def>();
+                StreamReader reader = null;
+                if (File.Exists(fPathSalesBRules))
+                {
+                    reader = new StreamReader(File.OpenRead(fPathSalesBRules));
+                    int count = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        if (count != 0)
+                        {
+
+                            var values = line.Split('#');
+                            Sales_MDR_Sales_BR_Def def = new Sales_MDR_Sales_BR_Def();
+                            for (int i = 0; i < values.Length; i++)
+                            {
+
+                                switch (i)
+                                {
+                                    case 0: // id
+                                        int idt = 0;
+                                        int.TryParse(values[0], out idt);
+                                        def.ID = idt;
+                                        break;
+                                    case 1: // code
+                                        def.Code = values[1];
+                                        break;
+                                    case 2: // endescription
+                                        def.EnDescription = values[2];
+                                        break;
+                                    case 3: //
+                                        def.ValidFrom = DateTime.Parse(values[3]);
+                                        break;
+                                    case 4: // 
+                                        def.ValidTo = DateTime.Parse(values[4]);
+                                        break;
+                                    case 5:
+                                        def.CreatedBy = values[5];
+                                        break;
+                                    case 6:
+                                        def.UpdatedBy = values[6];
+                                        break;
+                                    case 7:
+                                        def.CreatedOn = DateTime.Parse(values[7]);
+                                        break;
+                                    case 8:
+                                        try
+                                        {
+                                            def.UpdatedOn = DateTime.Parse(values[8]);
+                                        }
+                                        catch (Exception)
+                                        {
+                                            def.UpdatedOn = new DateTime(1970, 1, 1);
+                                        }
+
+                                        break;
+                                    case 9:
+                                        try
+                                        {
+                                            def.startdate = values[9];
+                                        }
+                                        catch (Exception)
+                                        {
+
+                                        }
+                                        break;
+                                    case 10:
+                                        def.enddate = values[10];
+                                        break;
+                                    case 11:
+                                        def.BR = values[11];
+                                        break;
+                                    case 12:
+                                        def.Level = values[12];
+                                        break;
+                                    case 13:
+                                        def.SubLevel = values[13];
+                                        break;
+                                    case 14:
+                                        def.Field = values[14];
+                                        break;
+                                    case 15:
+                                        def.EnMessage = values[15];
+                                        break;                                     
+                                }
+                            }
+                            SalesBrDef.Add(def);
+                        }
+                        count++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File doesn't exist");
+                }
+
+                #endregion
+
+                #endregion
+
+                foreach (var rule in SalesBrDef)
+                {
+                    switch (rule.Code)
+                    {
+                        case "....":
+
+                          
+
+                            break;
+                    }
+                }
+            }
 
 
             #region Commented XML XPATH Methods
