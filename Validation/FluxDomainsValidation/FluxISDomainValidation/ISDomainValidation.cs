ee3f4c3f3eae57e2e44bfd64d75991d819641e3b
kzhivkova <services.kzhivkova@gmail.com>
2023-09-19 09:05:03 +0300

    File Structure: Added Domains Directories with Classes

---
+++ Validation/FluxDomainsValidation/FluxISDomainValidation/ISDomainValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation.FluxDomainsValidation.FluxISDomainValidation
+{
+    class ISDomainValidation
+    {
+        public void ISDomainValidate(string strWorkPath)
+        {
+
+        }
+    }
+}
---
+++ Validation/FluxDomainsValidation/FluxVPDomainValidation/VPDomainValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation.FluxDomainsValidation.FluxVPDomainValidation
+{
+    class VPDomainValidation
+    {
+        public void VPDomainValidate(string strWorkPath)
+        {
+
+        }
+    }
+}
